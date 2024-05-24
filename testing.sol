d9387bdde01556bba9e6f714f463a3e5074b20c7
Kiseki16 <168454571+Kiseki16@users.noreply.github.com>
2024-05-25 02:23:25 +0800

    Update testing.sol

--- testing.sol
+++ testing.sol
@@ -1,1 +1,18 @@
+contract Testing{
+    string public tokenName = "Meta";
+    string public tokenAbriv = "MTA";
+    uint public totalSupply = 0;
 
+    mapping (address => uint) public balances;
+
+    function mint(address _address, uint _value)public{        
+        totalSupply += _value;
+        balances[_address] += _value; 
+    }
+
+    function burn(address _address, uint _value)public{       
+        if (balances[_address] >= _value) 
+        totalSupply -= _value;
+        balances[_address] -= _value; 
+    }
+}
